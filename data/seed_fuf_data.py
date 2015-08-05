0f026ad9c1b582ac42dd2220139d87062cc5a133
Kai <riselikethemoon@gmail.com>
2015-08-05 12:56:30 -0700

    still working on getting fuf database seeded

--- data/seed_fuf_data.py
+++ data/seed_fuf_data.py
@@ -6,22 +6,27 @@ from server import app
 
 def load_trees():
     """Load trees into database."""
-    
-    f = open("fuf_trees_trimmed")
+    print "I'm in load_trees()"
+    f = open("small_fuf_trees_trimmed.csv")
 
-    for line in f:
-        split_line = line.rstrip().split()
-        new_tree = Tree(user_id=split_line[0], 
+    new_line_f = list(f)[0].split('\r') # this is just the whole file for some terrible reason
+    for line in new_line_f: #splits into lines
+        # print line
+        split_line = line.split(',') # split that on commas
+        print "split line: %s" % split_line
+
+        new_tree = Tree(tree_id=split_line[0], 
 			        	tree_species_id=split_line[2], 
 			        	tree_plot_id=split_line[1])
-        db.session.add(new_tree)
+        print new_tree
+    #     db.session.add(new_tree)
 
-    db.session.commit()
+    # db.session.commit()
 
 def load_species():
     """Load species into database."""
     
-    f = open("fuf_species_trimmed")
+    f = open("fuf_species_trimmed.csv")
 
     for line in f:
         split_line = line.rstrip().split()
@@ -36,10 +41,12 @@ def load_species():
 def load_plots():
     """Load plots into database."""
     
-    f = open("fuf_plots_trimmed")
+    f = open("fuf_plots_trimmed.csv")
 
     for line in f:
+        print "***********************line: %s" % line
         split_line = line.rstrip().split()
+        print "***********************split line: %s" % split_line
         new_plot = Plot(plot_id=split_line[1], 
 			        	wkt=split_line[0],
 			        	address_street=split_line[2],
@@ -53,3 +60,7 @@ def load_plots():
 
 if __name__ == "__main__":
     connect_to_db(app)
+
+    load_trees()
+    # load_species()
+    # load_plots()
