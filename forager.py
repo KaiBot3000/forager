805918662b750b371b7df50f858f55393cd1b2d6
Kai <riselikethemoon@gmail.com>
2015-08-07 16:24:11 -0700

    moved some things around

--- forager.py
+++ forager.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from model import connect_to_db, db, Plant, User, Rating
 
 app=Flask(__name__)
 
@@ -9,9 +10,14 @@ app.secret_key = 'forage_the_things'
 @app.route('/')
 def index_page():
 
-	return render_template('forager-home.html')
+	return render_template('home-forager.html')
 
+@app.route('/map')
+def markers():
+		# get plant objects
+		# pass plant objects
 
+	return render_template('map-play.html') #and plant objects
 
 
 
