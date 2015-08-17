f083e7492cacc40fd3c17695fbd25a367122373a
Kai <riselikethemoon@gmail.com>
2015-08-17 16:50:04 -0700

    cleaned up unused code, comments

--- server.py
+++ server.py
@@ -9,17 +9,12 @@ app=Flask(__name__)
 
 app.secret_key = 'forage_the_things'
 
-# Query database
-# convert results to markers via Marker class
-# collect markers in feature collection via geojson
-# pass feature collection to map
-
-
 
 @app.route('/')
 def index_page():
 
 	return render_template('home-forager.html')
+	
 
 @app.route('/map')
 def markers():
@@ -33,19 +28,17 @@ def markers():
 
 	marker_collection = geojson.FeatureCollection(marker_list)
 
-	# new_marker = Marker(-122.411227, 37.772849, 'plant', 'plant from marker class!', 'park2')
-	# plant = geojson.dumps(new_marker, sort_keys=True)
-
 	return render_template('detail-play.html', marker_collection=marker_collection)
+
 
 @app.route('/plant-detail')
 def plant_details():
 	'''Gets marker/plant id from js, returns proper plant object.'''
-	# plant = Plant.query.get(plant_id)
 
 	plant_id = request.args.get('marker')
 	print plant_id
 	plant = Plant.query.get(plant_id)
+
 	# need to make desired plant attributes into dictionary, then JSONify dict and pass it.
 	# OR, pass completed html. Yes.
 	# Passing the object itself will result in errors because it came from SQLAlchemy and has methods attached.
@@ -60,7 +53,6 @@ def plant_details():
 	return detail_html
 
 
-
 if __name__ == "__main__":
 
 	connect_to_db(app)
@@ -68,6 +60,8 @@ if __name__ == "__main__":
 	app.run(debug=True)
 
 	DebugToolbarExtension(app)
+
+
 
 ### Graveyard ###
 
