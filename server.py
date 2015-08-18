927be1800ddf67743bad77f42602f185073be5b4
Kai <riselikethemoon@gmail.com>
2015-08-18 16:47:45 -0700

    search implemented. Some forms in place, data grabbed, query generates markers and passes them back to map

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ def markers():
 	marker_list = []
 
 	plants = Plant.query.all()
+
 	for plant in plants:
 		marker = Marker(plant.plant_lat, plant.plant_lon, plant.plant_id, plant.plant_name, plant.plant_description, 'park2')
 		marker_list.append(marker)
@@ -62,16 +63,6 @@ def list_fields():
 	names = db.session.query(Plant.plant_name).group_by(Plant.plant_name).all()
 	species = db.session.query(Plant.plant_species).group_by(Plant.plant_species).all()
 
-	# names_list = []
-
-	# for name in names:
-	# 	names_list.append(name[0])
-
-	# print names_list
-
-	# return json.dumps(names_list)
-
-	# combine
 	plants = names + species
 
 	# go through each and pull out of tuples
@@ -85,6 +76,83 @@ def list_fields():
 
 	# make into json string, pass back
 	return json.dumps(sorted_plants)
+
+
+@app.route('/search')
+def search_plants():
+	'''Takes search parameters, returns list of matching plants in geoJSON.
+	(may be able to combine this route with original /map route?)'''
+
+	# sample search string :/search?plants=all&category=FruitO&category=OtherT&season=Spring
+	plants = request.args.getlist('plant')
+	categories = request.args.getlist('category')
+	seasons = request.args.getlist('season')
+
+	# if no options selected, replace with list of all options possible
+
+	if plants == ['all']:
+		print 'changing plants\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+		names = db.session.query(Plant.plant_name).group_by(Plant.plant_name).all()
+		species = db.session.query(Plant.plant_species).group_by(Plant.plant_species).all()
+
+		plants_tuples = names + species
+
+		# go through each and pull out of tuples
+		plants = []
+
+		for plant in plants_tuples:
+			plant_name = str(plant[0])
+			plants.append(plant_name)
+
+	if categories == []:
+		categories = ['FruitO', 'FruitT', 'OtherT', 'Herb', 'Vegetable', 'tree']
+
+	# Initialize defaults for seasons
+	spring = [0, 1]
+	summer = [0, 1]
+	fall = [0, 1]
+	winter = [0, 1]
+
+	if seasons is not []:
+		if 'Spring' in seasons:
+			spring = [1]
+		if 'Summer' in seasons:
+			summer = [1]
+		if 'Fall' in seasons:
+			fall = [1]
+		if 'Winter' in seasons:
+			winter = [1]
+
+	##### Check things!
+	# print 'plants is: %s' % plants
+	# print type(plants[0])
+	# print type(plants)
+	# print 'categories is: %s' % categories
+	# print 'seasons is: %s' % seasons
+	# print 'spring is: %s' % spring
+	# print 'summer is: %s' % summer
+	# print 'fall is: %s' % fall
+	# print 'winter is: %s' % winter
+
+	marker_list = []
+	
+	# query for new plants
+	plant_objects = Plant.query.filter((Plant.plant_name.in_(plants)) | (Plant.plant_species.in_(plants))) \
+						.filter(Plant.plant_category.in_(categories)) \
+						.filter(Plant.plant_spring.in_(spring)) \
+						.filter(Plant.plant_summer.in_(summer)) \
+						.filter(Plant.plant_fall.in_(fall)) \
+						.filter(Plant.plant_winter.in_(winter))
+
+	# plants = Plant.query.all()
+
+	for plant in plant_objects:
+		marker = Marker(plant.plant_lat, plant.plant_lon, plant.plant_id, plant.plant_name, plant.plant_description, 'park2')
+		marker_list.append(marker)
+
+	marker_collection = geojson.FeatureCollection(marker_list)
+
+	return render_template('map.html', marker_collection=marker_collection)	
 
 
 if __name__ == "__main__":
