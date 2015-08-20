fdf045e68447984374317bd689114917773fd3fe
Kai <riselikethemoon@gmail.com>
2015-08-19 18:34:10 -0700

    worked on signup/in page and server routes

--- server.py
+++ server.py
@@ -32,6 +32,66 @@ def markers():
 
 	return render_template('map.html', marker_collection=marker_collection)
 
+@app.route('/sign', methods=['GET'])
+def sign():
+	'''Show sign in/up form'''
+
+	return render_template('sign.html')
+
+@app.route('/signin', methods=['POST'])
+def sign_in():
+	'''Sign in registered users'''
+
+	username = request.form['username']
+	password = request.form['password']
+
+	user = User.query.filter(username=username).first()
+
+	if not user:
+		flash('No such username! Please sign up or try again.')
+		return redirect('sign.html')
+
+	if user.password != password:
+		flash('Wrong password for that username! Please sign up or try again.')
+		return redirect('sign.html')
+
+	session['user_id'] = user.user_id
+
+	flash('Welcome back!')
+
+	return render_template('map.html')
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+	'''Sign up new users'''
+
+	username = request.form['username']
+	password1 = request.form['password1']
+	password2 = request.form['password2']
+
+	error = False
+
+	user = User.query.filter(username=username).first()
+
+	if user:
+		flash('That username is taken! Please try again')
+		error = True
+
+	if password1 != password2:
+		flash('Your passwords don\'t match! Please try again.')
+		error = True
+
+	if error:
+		return redirect('sign.html')
+	else:
+		new_user = User(username=username, password=password1)
+		db.session.add(new_user)
+		db.commit
+
+		flash('Welcome to Forager!')
+
+		return render_template('map.html')
+
 
 @app.route('/plant-detail')
 def plant_details():
@@ -76,6 +136,73 @@ def list_fields():
 
 	# make into json string, pass back
 	return json.dumps(sorted_plants)
+
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+    age = int(request.form["age"])
+    zipcode = request.form["zipcode"]
+
+    new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash("User %s added." % email)
+    return redirect("/")
+
+# pretty sure I can combine both log-in routes and just check methods
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Show login form."""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login."""
+
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+    return redirect("/users/%s" % user.user_id)
+
+
+@app.route('/logout')
+def logout():
+    """Log out."""
+
+    del session["user_id"]
+    flash("Logged Out.")
+    return redirect("/")
 
 
 @app.route('/search')
